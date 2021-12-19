586bc4cbb5bb84224ac1a43eb43931901ba18d01
myk4040okothogodo <65109328+myk4040okothogodo@users.noreply.github.com>
2021-12-19 19:15:42 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { Layout } from '../components';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
 
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
